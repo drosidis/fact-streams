71de6e25a0cb32f830a9b2a9f17880922af31cba
Yannis Drosidis <yannis@drosidis.com>
2023-08-28 05:34:13 +0100

    Setup tests for in-memory mongod instance

---
+++ test/mongo.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+
+import setupMongod from './setupMongod';
+import { Db } from 'mongodb';
+
+describe('mongodb-memory-server', () => {
+    let db: Db;
+    let stop: () => void;
+
+    beforeEach(async () => {
+        const setup = await setupMongod();
+        db = setup.db;
+        stop = setup.stop;
+    });
+
+    afterEach(async () => {
+        await stop();
+    });
+
+    it('should start an in-memory mongo instance', async () => {
+        // Before each test, the database should be empty
+        const before = await db.listCollections().toArray();
+        expect(before).to.be.an( "array" ).that.is.empty;
+
+        // Add a document at one collection
+        db.collection('myCollection').insertOne({
+            someValue: 100,
+        });
+
+        // The database should contain only the collection with the just added document
+        const after = await db.listCollections().toArray();
+        expect(after[0].name).to.eq('myCollection');
+    });
+});
---
+++ test/setupMongod.ts
@@ -0,0 +1,17 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongoClient, Db } from 'mongodb';
+
+export default async function setupMongod(): Promise<{ db: Db, stop: () => void }> {
+    // Create an new instance of "MongoMemoryServer" and automatically start it
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    const connection = await MongoClient.connect(uri);
+
+    return {
+        db: connection.db('fact_streams_test'),
+        stop: () => {
+            connection.close();
+            mongod.stop();
+        },
+    };
+}
